39912bc4f0e0679f133aafc6b43a9cb55f523cd6
Derek <dcostiga@umass.edu>
2014-12-02 14:17:10 -0500

    added functionality to create event button that sends pinn data to server

--- pinndit-app/public/javascripts/pinndit.js
+++ pinndit-app/public/javascripts/pinndit.js
@@ -7,6 +7,65 @@ var PinndItPin = {
     anchor: new google.maps.Point(0, 32)
 };
 
+function PinnClient(config){
+    for (var prop in config) {
+    this[prop] = config[prop];
+  }
+}
+
+PinnClient.prototype = {
+   pinnData : [],
+
+  // Request info from server
+  poll : function () {
+    var that = this;
+    this._stop = that.check();
+  },
+
+  // Stop requesting info
+  pollStop : function () {
+    clearInterval(this._stop);
+  },
+
+  // Post text to the server.
+  post : function (name, desc, k, B) {
+    $.ajax({
+      type : 'POST',
+      url  : '/post2',
+      data : { 'name' : name, 'desc' : desc, 'k' : k, 'B' : B},
+      dataType : 'json'
+    }).done(function (data) {
+      console.log('Post status: ' + data.status);
+    });
+  },
+
+  // Check for more messages on the server
+  // given the last index we have for the
+  // current posts.
+  // check : function () {
+  //   var that = this;    
+  //   $.ajax({
+  //     type : 'POST',
+  //     url  : '/check2',
+  //     data : { last : that.posts.length },
+  //     dataType : 'json'
+  //   }).done(function (data) {
+  //     console.log('Check rcvd: ' + JSON.stringify(data));
+
+  //     // Append the posts to the current posts:
+  //     that.posts = that.posts.concat(data);
+
+  //     // Rewrite to the view:
+  //     that.view.empty();
+  //     for (var i = 0; i < that.posts.length; i++) {
+  //       var li   = $('<li>');
+  //       li.html(that.posts[i].text);
+  //       that.view.append(li);
+  //     }
+  //   });
+  //} 
+};
+
 // A ChatClient object for communicating
 // with the chat server.
 function ChatClient(config) {
@@ -160,7 +219,7 @@ function success(position) {
 }
 
 function addNewPinn(location) {
-	
+
     console.log(location.toString());
 
     var pinnImage = '/images/PinndItPin50x50.png';
@@ -175,11 +234,11 @@ function addNewPinn(location) {
     map.setZoom(15);
 
     var contentString = '<div><p>New Pinn Information</p>' + 
-                        'Event Name: <input type="text" name="eventname"> <br>' + 
-                        'Event Description:  <input type="text" name="eventdescription"> <br>' + 
-                        '<button>Create Event</button>' + 
-                        '</form><br></div>' +
-                        '<div><input id= "submit" type="text" size="50">' +
+                        'Event Name: <input id = "event-name" type="text" name="event-name"> <br>' + 
+                        'Event Description:  <input id="event-description" type="text" name="event-description"> <br>' + 
+                        '<button name="create-event" id= "create-event" class="create-event">Create Event</button>' + 
+                        '<br></div>' +
+                        '<div>Comment: <input id= "submit" type="text" size="15">' +
                         '<button name="send" id= "send" class="send">Submit</button>' +
                         '<ul style="list-style: none" id="chat">' +
                         '</ul></div>';
@@ -193,12 +252,39 @@ function addNewPinn(location) {
     infowindow.open(map, pinn);
 
     google.maps.event.addListener(infowindow, 'domready', function() {
-        console.log("enteredTimeout\n");
         var chatc = new ChatClient({ view : $('ul#chat') });
         // Setup the post button:
+        chatc.poll();
         var postb = new PostButton({
-           view  : $('#send'),
-            input : $('#submit')
+            view   : $('#send'),
+            input  : $('#submit')
+        });
+
+        var pinnc = new PinnClient({    
+            view  : $('#event-name'),
+            view2 : $('#event-description')
+
+        });
+        var postb2 = new PostButton({
+            view    : $('#create-event'),
+            input   : $('#event-name'),
+            input2  : $('#event-description')
+        });
+
+        postb2.bind('click', function (event) {
+            console.log(this);
+            var text = this.input.val();
+            $('#event-name').prop('readonly', true);
+            var text2 = this.input2.val();
+            $('#event-description').prop('readonly', true);
+            $('#create-event').remove();
+            console.log(location.k);
+            console.log(location.B);
+            console.log(text);
+            console.log(text2);
+
+            pinnc.post(text, text2, location.k, location.B);
+            return false;
         });
 
         // Bind a click event:
--- pinndit-app/routes/index.js
+++ pinndit-app/routes/index.js
@@ -3,15 +3,33 @@ var router = express.Router();
 
 // Records all the posts made to the server.
 var posts = [];
+var pinnData = [];
 
 // Represents a post:
 function Post(text) {
   this.text = text;
   this.date = new Date();
 }
+
+function Pinn(eventname, eventdesc, eventk, eventB){
+	this.eventname = eventname;
+	this.eventdesc = eventdesc;
+	this.eventk = eventk;
+	this.eventB = eventB;
+}
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Pinndit' });
+});
+
+router.post('/post2', function (req, res) {
+	var eventname = req.body.name;
+	var descname = req.body.desc;
+	var k = req.body.k;
+	var B = req.body.B;
+	console.log('recieved post: ' + '(Name: ' + eventname + ') ' + '(Desc: ' + descname + ') ' + '(k: ' + k + ') ' + '(B: ' + B + ')');
+	pinnData.push(new Pinn(eventname, descname, k, B));
+	res.json({ status: 'OK'});
 });
 
 router.post('/post', function (req, res) {
