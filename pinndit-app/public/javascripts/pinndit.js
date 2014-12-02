06cc673fb8ba95ab8c2af3ca9fe5e36650da6e6b
Derek <dcostiga@umass.edu>
2014-12-01 20:59:06 -0500

    basic functionality of comments by creating a list and doing a post

--- pinndit-app/public/javascripts/pinndit.js
+++ pinndit-app/public/javascripts/pinndit.js
@@ -7,6 +7,83 @@ var PinndItPin = {
     anchor: new google.maps.Point(0, 32)
 };
 
+// A ChatClient object for communicating
+// with the chat server.
+function ChatClient(config) {
+  for (var prop in config) {
+    this[prop] = config[prop];
+  }
+}
+
+ChatClient.prototype = {
+  // An cache of posts received from server.
+  posts : [],
+
+  // Request info from server
+  poll : function () {
+    var that = this;
+    this._stop = that.check();
+  },
+
+  // Stop requesting info
+  pollStop : function () {
+    clearInterval(this._stop);
+  },
+
+  // Post text to the server.
+  post : function (text) {
+    $.ajax({
+      type : 'POST',
+      url  : '/post',
+      data : { 'text' : text},
+      dataType : 'json'
+    }).done(function (data) {
+      console.log('Post status: ' + data.status);
+    });
+  },
+
+  // Check for more messages on the server
+  // given the last index we have for the
+  // current posts.
+  check : function () {
+    var that = this;    
+    $.ajax({
+      type : 'POST',
+      url  : '/check',
+      data : { last : that.posts.length },
+      dataType : 'json'
+    }).done(function (data) {
+      console.log('Check rcvd: ' + JSON.stringify(data));
+
+      // Append the posts to the current posts:
+      that.posts = that.posts.concat(data);
+
+      // Rewrite to the view:
+      that.view.empty();
+      for (var i = 0; i < that.posts.length; i++) {
+        var li   = $('<li>');
+        li.html(that.posts[i].text);
+        that.view.append(li);
+      }
+    });
+  } 
+};
+
+function PostButton(config) {
+  for (var prop in config) {
+    this[prop] = config[prop];
+  }
+}
+
+PostButton.prototype = {
+  bind : function (type, cb) {
+    var that = this;
+    this.view.bind(type, function (event) {
+      cb.call(that, event);
+    });
+  }
+};
+
 function AddControlPinn(controlDiv, map) {
 
     controlDiv.style.padding = '15px';
@@ -97,18 +174,74 @@ function addNewPinn(location) {
     map.panTo(location);
     map.setZoom(15);
 
+    var contentObject = $('<div><p>New Pinn Information</p>' + 
+                        'Event Name: <input type="text" name="eventname"> <br>' + 
+                        'Event Description:  <input type="text" name="eventdescription"> <br>' + 
+                        '<button>Create Event</button>' + 
+                        '</form><br></div>' +
+                        '<div><input id= "submit" type="text" size="50">' +
+                        '<button name= "send" id= "send" class="send" onclick="pinndit:onSend()">Submit</button>' +
+                        '<ul style="list-style: none" id="chat">' +
+                        '</ul></div>');
+
+    var contentString = '<div><p>New Pinn Information</p>' + 
+                        'Event Name: <input type="text" name="eventname"> <br>' + 
+                        'Event Description:  <input type="text" name="eventdescription"> <br>' + 
+                        '<button>Create Event</button>' + 
+                        '</form><br></div>' +
+                        '<div><input id= "submit" type="text" size="50">' +
+                        '<button name="send" id= "send" class="send">Submit</button>' +
+                        '<ul style="list-style: none" id="chat">' +
+                        '</ul></div>';
 
     var infowindow = new google.maps.InfoWindow({
-        content: '<div><p>New Pinn Information</p>' + 
-                'Event Name: <input type="text" name="eventname"> <br>' + 
-                'Event Description:  <input type="text" name="eventdescription"> <br>' + 
-                '<button>Create Event</button>' + 
-                '</form></div>',
+        content: contentString,
         
         maxWidth: 500
     });
+
+    // infowindow.find("send").on('click', function() {
+    //     console.log("Send Button Works!\n");
+    // });
+
+    // var sendBtn   = contentObject.find('button.send')[0];
+    // google.maps.event.addDomListener(sendBtn, "click", function(event) {
+    //     pinn.setMap(null);
+    //     console.log("Send Button Works!\n");
+    // });
+    // function addComment(){
+    //     console.log("Test!\n");
+    // }
+    // google.maps.event.addListener(infowindow, 'domready', function() {
+    //     console.log("Inside New Listener!");
+    //     var chatc = new ChatClient({ view : document.id("chat") });
+    //     $(window).load(function(){
+    //     console.log('polling');
+    //     chatc.poll();
+    //     });
     
     infowindow.open(map, pinn);
+
+    setTimeout(function(){
+    console.log("enteredTimeout\n");
+    var chatc = new ChatClient({ view : $('ul#chat') });
+  // Setup the post button:
+    var postb = new PostButton({
+        view  : $('#send'),
+        input : $('#submit')
+    });
+
+  // Bind a click event:
+    postb.bind('click', function (event) {
+    console.log(this);
+    var text = this.input.val();
+    chatc.post(text);
+    $('#chat').append('<li>' + text + '</li>');
+    // clear input text:
+    this.input.val('');
+    return false;
+    });
+    },5000);
 
     google.maps.event.addListener(infowindow, 'closeclick', function() {
         pinn.setMap(null);
@@ -120,3 +253,4 @@ if (navigator.geolocation) {
 } else {
     error('Geo Location is not supported');
 }
+
--- pinndit-app/routes/index.js
+++ pinndit-app/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+// Records all the posts made to the server.
+var posts = [];
+
+// Represents a post:
+function Post(text) {
+  this.text = text;
+  this.date = new Date();
+}
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Pinndit' });
+});
+
+router.post('/post', function (req, res) {
+  var text = req.body.text;
+  console.log('received post: ' + text);
+  posts.push(new Post(text));
+  res.json({ status: 'OK'});
+});
+
+router.post('/check', function (req, res) {
+  var last = parseInt(req.body.last, 10);
+  var rest = posts.slice(last, posts.length);
+  res.json(rest);
 });
 
 module.exports = router;
