5757759a299dacdcdd1db2e7c59c0559072d8aa4
Ryan <rrherlih@umass.edu>
2014-12-03 18:10:22 -0500

    stylesheet update

--- pinndit-app/public/javascripts/pinndit.js
+++ pinndit-app/public/javascripts/pinndit.js
@@ -7,13 +7,14 @@ var controlPinn;
 //inactivePinn is undraggable pinn in top right corner when in the middle of creating new events Ariel
 var inActivePinn;
 var pinnformString = '<head> <link rel="stylesheet" href="/stylesheets/infoWindowStyle.css"/> </head>' +
-    '<div id = "iw"><p>Pinn Information</p>' +
+    '<div id = "iw"><p id="pinntitle"> New Pinn Information</p>' +
     '<input id = "event-name" type="text" name="event-name"> <br>' +
     '<input id="event-description" type="text" name="event-description"> <br>' +
     '<button name="create-event" id= "create-event" class="create-event">Create Event</button>' +
     '<br></div>';
 
-var pinnInfoString = '<div><p>Pinn Information</p>' +
+var pinnInfoString = '<head> <link rel="stylesheet" href="/stylesheets/infoWindowStyle.css"/> </head>' +
+    '<div id ="iw"><p id="pinntitle">Pinn Information</p>' +
     'Event Name: <input id="event-name" type="text" name="event-name" readonly> <br>' +
     'Event Description:  <input id="event-description" type="text" name="event-description" readonly> <br>' +
     '</div>' +
@@ -258,8 +259,8 @@ function addNewPinn(location) {
 
     var infowindow = new InfoBox({
         content: pinnformString,
-        pixelOffset: new google.maps.Size(-305, -215),
-        closeBoxMargin: "20px 0px 0px 0px",
+        pixelOffset: new google.maps.Size(-380, -100),
+        closeBoxMargin: "10px 155px 0px 0px",
         
         maxWidth: 500
     });
