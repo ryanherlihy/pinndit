7531e9ff80379ae1f130066ef0ebe31952641034
Dylan <drterry@umass.edu>
2014-12-04 11:30:40 -0500

    fixed setup code...

--- database/setup/dummyData.sql
+++ database/setup/dummyData.sql
@@ -1,9 +1,9 @@
 
 --Comments: CommentID, PinnID, Comment, Up, Down, SessionID, Time
 
-INSERT INTO "Comments" VALUES (1, 1, 'TestComment', 0, 0, 100, '2014-12-03 04:05:06');
-INSERT INTO "Comments" VALUES (2, 2, 'TestComment2', 1, 0, 100, '2014-12-03 04:05:06');
-INSERT INTO "Comments" VALUES (3, 3, 'TestComment3', 0, 10, 101, '2014-12-03 04:05:06');
+INSERT INTO "Comments" VALUES (DEFAULT, 1, 'TestComment', 0, 0, 100, '2014-12-03 04:05:06');
+INSERT INTO "Comments" VALUES (DEFAULT, 2, 'TestComment2', 1, 0, 100, '2014-12-03 04:05:06');
+INSERT INTO "Comments" VALUES (DEFAULT, 3, 'TestComment3', 0, 10, 101, '2014-12-03 04:05:06');
 
 --Pinns: PinnID, Active, Latitude, Longitude, EventName, Description, SessionID, Up, Down, Time
 
